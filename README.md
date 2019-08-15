7777ed9994e2721d2074c732b956f9b9f9fa0b39
dazhaozhao <446390206@qq.com>
2019-08-15 14:40:55 +0800

    配置数据库 配置基本配置

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Think5Admin
 基于thinkphp5.1框架和vue做的一个RBAC后台。
+
+##安装步骤
+1. composer install
+2. 导入数据库
+3. 编写一个.env文件，里面写上:
+```
+ENVIRONMENT=development
+DATABASE_HOST='127.0.0.1'
+DATABASE_USER=root
+DATABASE_PWD=123456
+DATABASE_DB=zhao
+APP_DEBUG=1
+APP_TRACE=1
+```
--- config/database.php
+++ config/database.php
@@ -13,23 +13,23 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => env('database_host'),
     // 数据库名
-    'database'        => '',
+    'database'        => env('database_db'),
     // 用户名
-    'username'        => 'root',
+    'username'        => env('database_user'),
     // 密码
-    'password'        => '',
+    'password'        => env('database_pwd'),
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => 'utf8mb4',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix'          => 'z_',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
