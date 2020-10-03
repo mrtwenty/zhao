3ce917e63449c4105493a644900c467bf47a2e63
dazhaozhao <446390206@qq.com>
2020-10-03 10:34:52 +0800

    提交一个函数

--- application/common.php
+++ application/common.php
@@ -11,6 +11,30 @@
 
 // 应用公共文件
 
+function cc_format($name)
+{
+    $temp_array = array();
+    for ($i = 0; $i < strlen($name); $i++) {
+        $ascii_code = ord($name[$i]);
+        if ($ascii_code >= 65 && $ascii_code <= 90) {
+            if ($i == 0) {
+                $temp_array[] = chr($ascii_code + 32);
+            } else {
+                $temp_array[] = '_' . chr($ascii_code + 32);
+            }
+        } else {
+            $temp_array[] = $name[$i];
+        }
+    }
+
+    return implode('', $temp_array);
+}
+
+function get_rule_url($con, $act)
+{
+    return cc_format($con) . '/' . $act;
+}
+
 /**
  * 输出select标签
  * @return [type] [description]
